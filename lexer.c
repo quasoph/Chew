2dda0edd052a4d7e96fd110031ca9d978ad6db63
Sophie Frankel <sophie.frankel@ukaea.uk>
2025-08-06 15:54:57 +0100

    Debugging

--- lexer.c
+++ lexer.c
@@ -21,6 +21,7 @@ Token *generate_keyword_or_identifier(char *current) {
         Token token : token with type, value and line_num.
     */
     char *keyword = malloc(sizeof(char) * 8);
+    keyword = current;
     Token *token = malloc(sizeof(Token));
     token->line_num = line_number;
 
@@ -102,33 +103,38 @@ Token lexer(FILE *file) {
         ----------
         Token *tokens : set of tokens generated.
     */
-    int num_tokens = 12;
+    int num_tokens = 0;
     int tokens_size = 0;
     int tokens_idx = 0;
 
     Token *tokens = malloc(sizeof(Token) * num_tokens);
 
     int current;
+    regex_t op_reegex;
+    regex_t sep_reegex;
+    int operator_rgx = regcomp(&op_reegex, "\\(|\\)|!|%|&|;|\\*", REG_EXTENDED);
+    int separator_rgx = regcomp(&sep_reegex, "[:blank:]", REG_EXTENDED);
 
     while ((current = fgetc(file)) != EOF) {
 
         char ch = (char) current;
-
         Token *token = malloc(sizeof(Token));
         
-        regex_t reegex;
-
-        int operator_rgx = regcomp(&reegex, "\\(|\\)|!|%|&|;|\\*", REG_EXTENDED);
-        operator_rgx = regexec(&reegex, &ch, 0, NULL, 0);
-        int separator_rgx = regcomp(&reegex, "[:blank:]", REG_EXTENDED);
-        separator_rgx = regexec(&reegex, &ch, 0, NULL, 0);
+        operator_rgx = regexec(&op_reegex, &ch, 0, NULL, 0);
+        separator_rgx = regexec(&sep_reegex, &ch, 0, NULL, 0);
 
         if (!operator_rgx) {
             token = generate_operator(&ch);
             printf("FOUND OPERATOR %c\n", ch);
+            num_tokens++;
+            tokens[tokens_idx] = *token;
+            tokens_idx++;
         } else if (!separator_rgx) {
             token = generate_separator(&ch);
             printf("FOUND SEPARATOR %c\n", ch);
+            num_tokens++;
+            tokens[tokens_idx] = *token;
+            tokens_idx++;
         } else if (ch == '"') {
             int cnt = 0;
             char current_word[20];
@@ -138,25 +144,31 @@ Token lexer(FILE *file) {
             }
             token = generate_string_literal(current_word);
             printf("FOUND STRING LITERAL %s\n", current_word);
+            num_tokens++;
+            tokens[tokens_idx] = *token;
+            tokens_idx++;
         } else if (isalpha(ch)) {
             int cnt = 0;
             char current_word[20];
             current_word[cnt] = ch;
-            while (isalpha(((char)(current = fgetc(file)))) && cnt < 20) {
+            while (isalpha(((current = fgetc(file)))) && cnt < 15) {
                 current_word[cnt+1] = (char) current;
-                cnt++;
+                cnt++; //need a line so that on the final character of the word, cnt++ does not happen
             }
             token = generate_keyword_or_identifier(current_word);
             printf("FOUND KEYWORD %s\n", current_word);
+            num_tokens++;
+            tokens[tokens_idx] = *token;
+            tokens_idx++;
         } else if (isdigit(ch)) {
             token = generate_int_literal(&ch);
             printf("FOUND INTEGER LITERAL %c\n", ch);
+            num_tokens++;
+            tokens[tokens_idx] = *token;
+            tokens_idx++;
         }
-        tokens[tokens_idx] = *token;
-        tokens_idx++;
-        num_tokens++;
     }
-    tokens[tokens_idx].value = 0;
-    tokens[tokens_idx].type = EOT;
+    regfree(&op_reegex);
+    regfree(&sep_reegex);
     return *tokens;
 }
